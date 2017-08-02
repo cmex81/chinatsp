909600edb9ee8e6f42d0d24f5f9aa5e26afe630a
ryanchen <ryan.chen@harman.com>
2017-08-02 11:39:21 +0800

    update storage framework

---
+++ TMiscService/app/src/main/java/com/chinatsp/misc/IServiceInterface.java
@@ -0,0 +1,12 @@
+package com.chinatsp.misc;
+
+import android.content.Context;
+
+/**
+ * Created by ryan on 02/08/2017.
+ */
+public interface IServiceInterface{
+
+     void initService(Context context);
+     void onDestroy();
+}
